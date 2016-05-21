4563a98fa6396720a0a71b265da9be224519f8f7
Shin <mrmaxin@gmail.com>
2016-05-21 15:30:21 -0700

    add ingredient form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe < ActiveRecord::Base
 	accepts_nested_attributes_for :ingredients,
 									reject_if: proc { |attributes| attributes['name'].blank? },
 									allow_destroy: true
+									
 	accepts_nested_attributes_for :directions,
 									reject_if: proc { |attributes| attributes['step'].blank? },
 									allow_destroy: true
